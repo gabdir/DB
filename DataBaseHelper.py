28f914d1b9099e8c11dcc72e1615956a39ff5a3e
Aidar <aidarsuperboy2312@gmail.com>
2018-11-21 23:41:51 +0300

    DB

--- DataBaseHelper.py
+++ DataBaseHelper.py
@@ -13,22 +13,61 @@ def create_tables():
         """,
         """ 
         CREATE TABLE Station (
-                UID SERIAL PRIMARY KEY,
-                Time_charing INTEGER NOT NULL,
-                Location VARCHAR(255) NOT NULL,
-                Price_of_charging INTEGER NOT NULL,
-                Plug_formats VARCHAR(255) NOT NULL,
-                Free_sockets INTEGER NOT NULL,
-                Amount_of_sockets INTEGER NOT NULL,
-                company VARCHAR(255) REFERENCES Company(Company_id)
-                )
-        """)
+            UID SERIAL PRIMARY KEY,
+            Time_charing INTEGER NOT NULL,
+            Location VARCHAR(255) NOT NULL,
+            Price_of_charging INTEGER NOT NULL,
+            Plug_formats VARCHAR(255) NOT NULL,
+            Free_sockets INTEGER NOT NULL,
+            Amount_of_sockets INTEGER NOT NULL,
+            company SERIAL REFERENCES Company(Company_id) ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        """,
+        """
+        CREATE TABLE Car (
+            Identification_num SERIAL PRIMARY KEY,
+            Model VARCHAR(255) NOT NULL,
+            Status VARCHAR(255) NOT NULL,
+            Location SERIAL NOT NULL,
+            c_company SERIAL NOT NULL,
+            FOREIGN KEY (c_company) REFERENCES Company(Company_id) ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        """,
+        """
+        CREATE TABLE Workshops (
+            WID SERIAL PRIMARY KEY,
+            Location VARCHAR(255) NOT NULL,
+            Available_car_p VARCHAR(255) NOT NULL,
+            Available_time DOUBLE PRECISION NOT NULL,
+            w_company SERIAL NOT NULL,
+            FOREIGN KEY (w_company) REFERENCES Company(Company_id) ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        """,
+        """
+        CREATE TABLE Provider (
+            PCompany_id SERIAL PRIMARY KEY,
+            Phone_num VARCHAR(10) NOT NULL,
+            Address VARCHAR(255) NOT NULL,
+            Name VARCHAR(30) NOT NULL
+        )
+        """,
+        """
+        CREATE TABLE Payment (
+            PCompany_id SERIAL,
+            Company_id SERIAL,
+            FOREIGN KEY (Company_id) REFERENCES Company(Company_id) ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (PCompany_id) REFERENCES Provider(PCompany_id) ON UPDATE CASCADE ON DELETE CASCADE     
+        )
+        """
+    )
     conn = None
     try:
         conn = psycopg2.connect("dbname='postgres' user='test' host='10.90.138.41' password='test'")
         cur = conn.cursor()
+
         for command in commands:
             cur.execute(command)
+
         cur.close()
 
         conn.commit()
