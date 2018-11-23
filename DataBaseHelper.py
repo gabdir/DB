9ecb126ff1380b90a73a1cd6df1b7997745b081a
Albert Badretdinov <al.badretdinov@innopolis.ru>
2018-11-23 22:06:24 +0300

    Function for providing sample data

--- DataBaseHelper.py
+++ DataBaseHelper.py
@@ -1,4 +1,3 @@
-
 import psycopg2
 
 
@@ -116,7 +115,7 @@ def create_tables():
         )
         """,
         """
-        CREATE TABLE History (
+        CREATE TABLE History_of_trip (
             Identification_num SERIAL,
             Username VARCHAR(30),
             starting_loc SERIAL,
@@ -159,6 +158,7 @@ def create_tables():
         if conn is not None:
             conn.close()
 
+
 def delete_tables():
     command = """
     DROP TABLE 
@@ -171,7 +171,7 @@ def delete_tables():
     Customer, 
     History_of_providing, 
     History_of_charging,
-    History, 
+    History_of_trip, 
     History_of_repairing   CASCADE 
     """
     conn = None
@@ -192,5 +192,73 @@ def delete_tables():
         if conn is not None:
             conn.close()
 
+
+def input_sample_data():
+    commands = ("""
+    INSERT INTO company (company_id, company_name) 
+    VALUES ('Jane', '1935-08-01')
+    """,
+                """
+                INSERT INTO station (time_charing, location, price_of_charging, plug_formats, free_sockets, amount_of_sockets, company) 
+                values ('Jane', '1935-08-01')
+                """,
+                """
+                INSERT INTO workshops (location, available_car_p, available_time, w_company) 
+                values ('Jane', '1935-08-01')
+                """,
+                """
+                INSERT INTO provider (phone_num, address, name) 
+                values ('Jane', '1935-08-01')
+                """,
+                """
+                INSERT INTO payment (company_id, company_name) 
+                values ('Jane', '1935-08-01')
+                """,
+                """
+                INSERT INTO car (model, uid, status, location, c_company) 
+                values ('Jane', '1935-08-01')
+                """,
+                """
+                    INSERT INTO customer (email, phone_number, location, full_name) 
+                    values ('Jane', '1935-08-01')
+                    """,
+                """
+                    INSERT INTO history_of_providing (wid, pcompany_id, type_car_p) 
+                    values ('Jane', '1935-08-01')
+                    """,
+                """
+                    INSERT INTO history_of_charging (identification_num, uid, starting_ch, ending_ch, price) 
+                    values ('Jane', '1935-08-01')
+                    """,
+                """
+                    INSERT INTO history_of_trip (identification_num, username, starting_loc, client_loc, final_loc) 
+                    values ('Jane', '1935-08-01')
+                    """,
+                """
+                    INSERT INTO history_of_repairing (identification_num, wid, price, car_parts, data) 
+                    values ('Jane', '1935-08-01')
+                    """,
+
+                )
+    conn = None
+    try:
+        conn = psycopg2.connect("dbname='postgres' user='test' host='10.90.138.41' password='test'")
+        cur = conn.cursor()
+
+        '''''for command in commands2:
+            cur.execute(command)'''''
+        cur.execute(commands)
+
+        cur.close()
+
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 if __name__ == '__main__':
-        create_tables()
+    delete_tables()
+    create_tables()
